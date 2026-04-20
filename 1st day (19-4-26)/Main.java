e54a9d69b1727f979d6211a1c39dd4d9625c6009
rashsed60cyber <rashsed60@gmail.com>
2026-04-20 09:16:48 +0600

    Create Main.java

---
+++ 1st day (19-4-26)/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+  Car myCar=new Car();
+  myCar.color="Black";
+  myCar.model="Toyota";
+  myCar.year=2025;
+  myCar.displayInfo();
+    }
+}
